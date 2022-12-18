1c09111c2e96d8fefb0ab4d627e55d518af2c3f9
danich777 <112581596+danich777@users.noreply.github.com>
2022-12-18 21:21:21 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,42 +1,39 @@
-string = "2 + 9 * 10 - 2"
+def parser(string):
+    exp_list = string.split()
+    print(exp_list)
+    for i in range(len(exp_list)):
+            if exp_list[i].isdigit():
+                exp_list[i] = int(exp_list[i])
+    result = 0
+    while len(exp_list) != 1:
+        i = 0
+        while ('*' in exp_list or '/' in exp_list) and i < len(exp_list):
+            if exp_list[i] == '*':
+                result = exp_list[i - 1] * exp_list[i + 1]
+                exp_list.pop(i)
+                exp_list.pop(i)
+                exp_list[i - 1] = result
+            elif exp_list[i] == '/':
+                result = exp_list[i - 1] / exp_list[i + 1]
+                exp_list.pop(i)
+                exp_list.pop(i)
+                exp_list[i - 1] = result
+            else:
+                i += 1
 
-list = string.split()
-for i in range(len(list)):
-    if list[i].isdigit():
-        list[i] = int(list[i])
-print(list)
-
-result = 0
-
-while len(list) != 1:
-    i = 0
-    while ('*' in list or '/' in list) and i < len(list):
-        if list[i] == '*':
-            result = list[i-1] * list [i+1]
-            list.pop(i)
-            list.pop(i)
-            list[i-1] = result
-        elif list[i] == '/':
-            result = list[i-1] / list [i+1]
-            list.pop(i)
-            list.pop(i)
-            list[i-1] = result
-        else:
-            i += 1
-
-    while ('+' in list or '-' in list) and i < len(list):
-        if list[i] == '+':
-            result = list[i-1] + list [i+1]
-            list.pop(i)
-            list.pop(i)
-            list[i-1] = result
-            i -= 1
-        elif list[i] == '-':
-            result = list[i-1] - list [i+1]
-            list.pop(i)
-            list.pop(i)
-            list[i-1] = result
-            i -= 1
-        else:
-            i += 1
-        print(list)
+        while ('+' in exp_list or '-' in exp_list) and i < len(exp_list):
+            if exp_list[i] == '+':
+                result = exp_list[i - 1] + exp_list[i + 1]
+                exp_list.pop(i)
+                exp_list.pop(i)
+                exp_list[i - 1] = result
+                i -= 1
+            elif exp_list[i] == '-':
+                result = exp_list[i - 1] - exp_list[i + 1]
+                exp_list.pop(i)
+                exp_list.pop(i)
+                exp_list[i - 1] = result
+                i -= 1
+            else:
+                i += 1
+    print(f'Результат: {result}')
